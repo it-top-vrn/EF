e6ee1cc4cb4c6372085ce3ab68d3b2ae1203c354
Starinin Andrey <starininandrey@gmail.com>
2021-12-16 20:48:03 +0300

    Add AuthorsCrud.cs

---
+++ EF.Lib/CRUD/AuthorsCrud.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using EF.Model;
+
+namespace EF.Lib.CRUD
+{
+    public class AuthorsCrud : ICrud<Author>, IDisposable
+    {
+        private readonly BookStore _db;
+
+        public AuthorsCrud()
+        {
+            _db = BookStore.Init();
+        }
+        
+        public void Add(Author obj)
+        {
+            _db.TableAuthors.Add(obj);
+            _db.SaveChanges();
+        }
+
+        public void Update(Author obj)
+        {
+            _db.TableAuthors.Update(obj);
+            _db.SaveChanges();
+        }
+
+        public void Delete(Author obj) { }
+
+        public List<Author> ReadAll()
+        {
+            return _db.TableAuthors.ToList();
+        }
+
+        public void Dispose()
+        {
+            _db?.Dispose();
+        }
+    }
+}
