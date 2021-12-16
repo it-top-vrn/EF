e9ccf6789a241a201221fad0191c6074a8333c50
Starinin Andrey <starininandrey@gmail.com>
2021-12-16 20:39:21 +0300

    Update

---
+++ EF.Lib/CRUD/ICrud.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace EF.Lib.CRUD
+{
+    public interface ICrud<T>
+    {
+        public void Add(T obj);
+        public void Update(T obj);
+        public void Delete(T obj);
+        public List<T> ReadAll();
+    }
+}
