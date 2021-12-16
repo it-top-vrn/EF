2c916987f11f1d474004abf26831b2b8329f028e
Starinin Andrey <starininandrey@gmail.com>
2021-12-16 19:40:09 +0300

    Add DBContext

---
+++ EF.Lib/BookStore.cs
@@ -0,0 +1,20 @@
+﻿using EF.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF.Lib
+{
+    public sealed class BookStore : DbContext
+    {
+        public DbSet<Author> TableAuthors { get; set; }
+
+        public BookStore()
+        {
+            Database.EnsureCreated();
+        }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=mysql60.hostland.ru;user=host1323541_itstep;password=269f43dc;database=host1323541_vrn07;");
+        }
+    }
+}
